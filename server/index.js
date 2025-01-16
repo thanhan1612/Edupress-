bb93cb51dd06f443d7b467a32bdd30c6273bf928
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2025-01-16 22:55:04 +0700

    .

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(errorMiddlewares);
+
 app.use('/api/users', userRouters)
 
 
-
+app.use(errorMiddlewares);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
