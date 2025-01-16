6aa68e0c6c619e86a1be79faef006d39c994f210
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2025-01-16 22:43:56 +0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+import HomePage from './Pages/HomePage/HomePage';
+import SignUpPage from './Pages/SignUpPage/SignUpPage';
+import SignInPage from './Pages/SignInPage/SignInPage';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-    </Routes>
-    </>
+   
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Router>
     
-  )
+  );
 }
 
-export default App
+export default App;
