d997dcaa68ec5a0edde2e3556fe637cedffd2f1e
DuyNgo <baoduy.d17@gmail.com>
2025-03-02 17:08:33 +0700

    update BlogCard to BlogList, update Route with BlogLayout page and Blog detail with ID. TODO: pagination in Blog List + Blog Detail Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,14 +8,82 @@ import OTPverify from "./Pages/OTPsend/OTPsend";
 import Reset from "./Pages/OTPsend/Reset";
 import { createContext, useState } from "react";
 import Courses from "./Pages/AllCourses/AllCourses";
-import BlogListing from "./Pages/BlogListing/BlogListing";
+import BlogListingLayout from "./Pages/BlogListing/BlogListingLayout";
+import BlogDetail from "./Pages/BlogListing/BlogDetail";
+import BlogList from "./Pages/BlogListing/BlogComponents/BlogList";
 
 export const RecoveryContext = createContext();
+
+const blogs = [
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Commercial",
+    tag: ["Marketing", "Idea", "LMS"],
+  },
+
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Shop",
+    tag: ["Marketing", "LearnPress", "Instructor"],
+  },
+
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Educate",
+    tag: ["LearnPress", "LLMS", "Idea"],
+  },
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Commercial",
+    tag: ["Marketing", "Idea", "LMS"],
+  },
+
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Shop",
+    tag: ["Marketing", "LearnPress", "Instructor"],
+  },
+
+  {
+    title: "random",
+    description: "desc",
+    date: "dd/mm/yyyy",
+    category: "Educate",
+    tag: ["LearnPress", "LLMS", "Idea"],
+  },
+];
+
 function App() {
   const [page, setPage] = useState("/signin");
   const [email, setEmail] = useState("");
   const [otp, setOTP] = useState();
 
+  // BLOG PAGE
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const toggleCategories = (category) => {
+    setSelectedCategories((prev) =>
+      prev.includes(category)
+        ? prev.filter((item) => item !== category)
+        : [...prev, category]
+    );
+  };
+  const toggleTags = (tag) => {
+    setSelectedTags((prev) =>
+      prev.includes(tag) ? prev.filter((item) => item !== tag) : [...prev, tag]
+    );
+  };
+  // END OF BLOG PAGE________________________________________________________________
   return (
     <RecoveryContext.Provider
       value={{ setPage, page, otp, setOTP, setEmail, email }}>
@@ -28,7 +96,30 @@ function App() {
           <Route path="/reset" element={<Reset />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/homepage/contact" element={<Contact />} />
-          <Route path="/blog" element={<BlogListing />} />
+          <Route
+            path="/blog"
+            element={
+              <BlogListingLayout
+                selectedCategories={selectedCategories}
+                setSelectedCategories={setSelectedCategories}
+                selectedTags={selectedTags}
+                setSelectedTags={setSelectedTags}
+                toggleCategories={toggleCategories}
+                toggleTags={toggleTags}
+              />
+            }>
+            <Route
+              index
+              element={
+                <BlogList
+                  blogs={blogs}
+                  selectedCategories={selectedCategories}
+                  selectedTags={selectedTags}
+                />
+              }
+            />
+            <Route path=":id" element={<BlogDetail />} />
+          </Route>
         </Routes>
       </Router>
     </RecoveryContext.Provider>
